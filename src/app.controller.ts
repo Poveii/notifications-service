1ab47865cf7821c555b22208aed0c26215426d50
Poveii <yellowtutors@gmail.com>
2022-12-14 13:31:06 -0300

    :ok_hand: style: Changed to double quote

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-import { randomUUID } from 'node:crypto';
-import { CreateNotificationBody } from './create-notification-body';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
+import { randomUUID } from "node:crypto";
+import { CreateNotificationBody } from "./create-notification-body";
 
-@Controller('notifications')
+@Controller("notifications")
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
