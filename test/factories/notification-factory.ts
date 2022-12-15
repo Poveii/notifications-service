666a4b49e8c9a4d16668435561d5d274821e1825
Poveii <yellowtutors@gmail.com>
2022-12-15 17:21:52 -0300

    :ok_hand: style: MakeNotification Name to camelCase

--- test/factories/notification-factory.ts
+++ test/factories/notification-factory.ts
@@ -6,7 +6,7 @@ import {
 
 type Override = Partial<NotificationProps>;
 
-export function MakeNotification(override: Override = {}) {
+export function makeNotification(override: Override = {}) {
   return new Notification({
     category: "social",
     content: new Content("Nova solicitação de amizade!"),
